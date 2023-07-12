3ca367363b5a44519ffeede28bf6994239e7bf79
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-07-12 20:59:00 +0100

    database

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,7 @@
+-- script that creates a table users with follwing fields
+-- id, email, name
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER NOT NULL PRIMARY KEY,
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255
 );
