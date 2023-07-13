fa4eab06c990997b8238faa79ea4319412bbb822
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-07-13 18:00:03 +0100

    ADVANCED

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,5 @@
--- This SQL script select band_name, and lifespan column which is difference
+-- script that lists all bands with Glam rock as their main style, ranked by their longevity
+-- Import this table dump: metal_bands.sql.zip
 SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
     FROM metal_bands
     WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
