9d42549496ca9a36081cc3b2c5f20ec7e3047f73
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-07-19 21:02:19 +0100

    pycharm

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """
 Create a Cache class.
-In the __init__ method, store an instance of the Redis client as a private variable 
+In the __init__ method, store an instance of the
+Redis client as a private variable
 named _redis (using redis.Redis()) and flush the instance using
 """
 import redis
@@ -21,8 +22,9 @@ class Cache:
 
     def store(self, data: Union[str, float, int, bytes]) -> str:
         """
-        Create a store method that takes a data argument and 
-        returns a string
+
+        :param data:
+        :return:
         """
         randKey = str(uuid.uuid4())
         self._redis.set(randKey, data)
